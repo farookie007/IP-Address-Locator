94355c157d30fc23ee5a392e287b0885c9a0e38f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-03-22 22:50:14 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# IP-Address-Locator
+
+This program gets the IP Address of the computer or
+machine on which it is launched and returns a nicely 
+formatted message of the machine's information.
+
+The message will display the `Location`, `ISP`,
+`Country`, `City`, `IP Address` and other useful
+information about the IP Address.
--- app.py
+++ app.py
@@ -1,21 +1,22 @@
+import requests
+
 from functions import getPublicIP
 
+if __name__ == '__main__':
+	IP_addr = getPublicIP()
 
-IP = getPublicIP()
+	# Sending the request and getting the json
+	url = f'http://ipinfo.io/{IP_addr}/json'
+	response = requests.get(url)
+	data = response.json()
 
-# Getting the Details
-url = 'http://ipinfo.io/' + IP + '/json'
-response = urlopen(url)
-data = json.load(response)
-city = data['city']
-region = data['region']
-country = data['country']
-location = data['loc']
-org = data['org']
-
-# Displaying the Extracted Details
-print("Your City : ".ljust(20) + city.ljust(20))
-print("Your Region : ".ljust(20) + region.ljust(20))
-print("Your Country : ".ljust(20) + country.ljust(20))
-print("Your Location : ".ljust(20) + location.ljust(20))
-print("Your IP Address: ".ljust(20) + IP.ljust(20))
+	# Extracting the Results
+	print('###############################################')
+	print("Your City : ".ljust(20) + data['city'].ljust(20))
+	print("Your Region : ".ljust(20) + data['region'].ljust(20))
+	print("Your Country : ".ljust(20) + data['country'].ljust(20))
+	print("Your Location : ".ljust(20) + data['loc'].ljust(20))
+	print("Your IP Address: ".ljust(20) + data['ip'].ljust(20))
+	print("Your ISP:".ljust(20) + data['org'].ljust(20))
+	print("Your Timezone: ".ljust(20) + data['timezone'].ljust(20))
+	print('###############################################')
